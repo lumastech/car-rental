4dc561827628fb8f80ce8ae8741dbfccfff0e3ae
Lumas <lumastech@gmail.com>
2023-09-24 19:26:17 +0200

    added car, booking models, added car, booking migration table. added resource functions in car controller

---
+++ app/Models/Car.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    use HasFactory;
+    protected $fillable = ['make', 'model', 'year', 'price_per_day', 'availability'];
+
+    // Define relationships here if needed
+}
--- database/migrations/2023_09_24_171419_create_cars_table.php
+++ database/migrations/2023_09_24_171419_create_cars_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cars', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['customer', 'admin', 'driver']);
-            $table->rememberToken();
+            $table->string('make');
+            $table->string('model');
+            $table->integer('year');
+            $table->decimal('price_per_day', 10, 2);
+            $table->boolean('availability');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cars');
     }
 };
